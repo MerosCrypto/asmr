4439f18a321381510b9ec26f0569296c63eb4fd9
Luke Parker <lukeparker5132@gmail.com>
2021-06-07 03:33:49 -0500

    Add support for Ristretto

--- src/tests/dl_eq.rs
+++ src/tests/dl_eq.rs
@@ -1,8 +1,9 @@
 use crate::{
   crypt_engines::CryptEngine,
   crypt_engines::{
+    secp256k1_engine::Secp256k1Engine,
     ed25519_engine::Ed25519Sha,
-    secp256k1_engine::Secp256k1Engine
+    ristretto_engine::RistrettoEngine
   },
   dl_eq::DlEqProof
 };
@@ -35,6 +36,20 @@ fn dl_eq_secp256k1_with_ed25519() {
 }
 
 #[test]
+fn dl_eq_secp256k1_with_ristretto() {
+  let _ = env_logger::builder().is_test(true).try_init();
+  test_dl_eq_with_engines::<Secp256k1Engine, RistrettoEngine>();
+  test_dl_eq_with_engines::<RistrettoEngine, Secp256k1Engine>();
+}
+
+#[test]
+fn dl_eq_ed25519_with_ristretto() {
+  let _ = env_logger::builder().is_test(true).try_init();
+  test_dl_eq_with_engines::<Ed25519Sha, RistrettoEngine>();
+  test_dl_eq_with_engines::<RistrettoEngine, Ed25519Sha>();
+}
+
+#[test]
 fn test_max_key_wrapping() {
   let _ = env_logger::builder().is_test(true).try_init();
   let mut key = [0xffu8; 32];
@@ -42,6 +57,7 @@ fn test_max_key_wrapping() {
   key[31] = 0b0000_1111;
   let mut key_rev = key;
   key_rev.reverse();
-  assert_eq!(Ed25519Sha::private_key_to_bytes(&Ed25519Sha::little_endian_bytes_to_private_key(key).unwrap()), key);
   assert_eq!(Secp256k1Engine::private_key_to_bytes(&Secp256k1Engine::little_endian_bytes_to_private_key(key).unwrap()), key_rev);
+  assert_eq!(Ed25519Sha::private_key_to_bytes(&Ed25519Sha::little_endian_bytes_to_private_key(key).unwrap()), key);
+  assert_eq!(RistrettoEngine::private_key_to_bytes(&RistrettoEngine::little_endian_bytes_to_private_key(key).unwrap()), key);
 }
