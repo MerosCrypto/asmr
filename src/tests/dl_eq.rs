d329f1d9834e6c41176044b67308caac60bddea5
Luke Parker <lukeparker5132@gmail.com>
2021-06-07 03:38:01 -0500

    Add missing Jubjub test
    We didn't test the highest possible key could be handled, and didn't 
    update the constant used to create such a key.

--- src/tests/dl_eq.rs
+++ src/tests/dl_eq.rs
@@ -60,9 +60,10 @@ fn test_max_key_wrapping() {
   let _ = env_logger::builder().is_test(true).try_init();
   let mut key = [0xffu8; 32];
   assert_eq!(crate::dl_eq::SHARED_KEY_BITS, 251); // Change the following line if this changes
-  key[31] = 0b0000_1111;
+  key[31] = 0b0000_0111;
   let mut key_rev = key;
   key_rev.reverse();
   assert_eq!(Ed25519Sha::private_key_to_bytes(&Ed25519Sha::little_endian_bytes_to_private_key(key).unwrap()), key);
   assert_eq!(Secp256k1Engine::private_key_to_bytes(&Secp256k1Engine::little_endian_bytes_to_private_key(key).unwrap()), key_rev);
+  assert_eq!(JubjubEngine::private_key_to_bytes(&JubjubEngine::little_endian_bytes_to_private_key(key).unwrap()), key);
 }
